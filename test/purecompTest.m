8534babccec5bcc18a564b677b04d8110a7fa9e4
Sho Hirose <sho.hirose@gmail.com>
2023-08-22 22:55:00 +0900

    Update purecompTest

--- test/purecompTest.m
+++ test/purecompTest.m
@@ -13,23 +13,24 @@ T = 180;        % Temperature [K]
 
 %% VanDerWaalsEos test:
 eos = VanDerWaalsEos(Pc,Tc,Mw);
-[z,s] = eos.zFactors(P,T);
-phi = eos.fugacityCoeff(z,s);
+result = eos.zFactors(P,T);
+phi = eos.fugacityCoeff(result);
+z = result.z;
 assert(abs((z - 0.6999)/z) < 1e-3);
 assert(abs((phi - 0.7769)/phi) < 1e-3);
 
 %% SoaveRedlichKwongEos test:
 eos = SoaveRedlichKwongEos(Pc,Tc,omega,Mw);
-[z,s] = eos.zFactors(P,T);
-z = sort(z);
-phi = eos.fugacityCoeff(z,s);
+result = eos.zFactors(P,T);
+z = result.z;
+phi = eos.fugacityCoeff(result);
 assert(max(abs((z - [0.1393, 0.2132, 0.6475]')./z)) < 1e-3);
 assert(max(abs((phi - [0.7808, 0.7863, 0.7441]')./phi)) < 1e-3);
 
 %% PengRobinsonEos test:
 eos = PengRobinsonEos(Pc,Tc,omega,Mw);
-[z,s] = eos.zFactors(P,T);
-z = sort(z);
-phi = eos.fugacityCoeff(z,s);
+result = eos.zFactors(P,T);
+z = result.z;
+phi = eos.fugacityCoeff(result);
 assert(max(abs((z - [0.1237, 0.1984, 0.6242]')./z)) < 1e-3);
 assert(max(abs((phi - [0.7595, 0.7657, 0.7257]')./phi)) < 1e-3);
